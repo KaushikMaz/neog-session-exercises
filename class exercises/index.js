76bd16e5b4b16a658439bdf858655adaaab43f52
Kaushik <kmzdr1@gmail.com>
2022-06-25 17:25:39 +0530

    update:exercise 2

--- class exercises/index.js
+++ class exercises/index.js
@@ -3,3 +3,11 @@
 const obj={Mother:"Pampa",age:27}
 const shruti={...obj,age:obj.age-10}
 console.log(shruti)
+
+
+// Take an array for five colors.
+// Create another array by adding two more colors to it.
+
+const colors=["pink","blue","grey","black","yellow"];
+const newColors=[...colors,"red","green"];
+console.log(newColors)
