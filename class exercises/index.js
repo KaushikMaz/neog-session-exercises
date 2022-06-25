531c1dd9fb6bc7fc1853801ae873a6f41d5fa193
Kaushik <kmzdr1@gmail.com>
2022-06-25 23:00:56 +0530

    update: filter exercise

--- class exercises/index.js
+++ class exercises/index.js
@@ -16,3 +16,11 @@ console.log(newColors)
 // and increase age
  const Birthday=(person)=>({...person,age:person.age+1})
  console.log(Birthday(obj))
+
+//  Write a function which can tell if a number is less than 10 or not. Supply
+//  this function to an Array to ensure that there is no number less than 10
+
+const array=[2,3,52,55,,7,1,34,23,12]
+const newArray=array.filter(x=>x>10)
+console.log(newArray)
+
